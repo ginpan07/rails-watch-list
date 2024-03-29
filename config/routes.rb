0dbeb388cdc3bbe93de46564a8236b77279ba046
ginpan <zgpan7@gmail.com>
2024-03-29 16:09:10 +1100

    Hope for the best

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
 
   resources :lists do
     resources :bookmarks, only: [:new, :create]
-    resources :movies, only: [:new, :create]
   end
 
   resources :bookmarks, only: [:destroy]
